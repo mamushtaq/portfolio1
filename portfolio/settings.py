b971c6b15309dc40d07194debded00314ec5f699
Ali <alimushtaq403@gmail.com>
2021-09-14 01:20:37 +0500

    httpresponse redirect added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,6 +134,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
